28393a7168387fa179c4a9bbbe9ec7dee1cd512b
Ryan Pecha <ryanpecha@aol.com>
2022-11-10 09:05:02 -0700

    added filePath argument to main

--- Main.py
+++ Main.py
@@ -6,14 +6,28 @@ def main():
     import matplotlib.pyplot as plt
     import pandas as pd
     import os
-
-    filePath = 'sampleIntegrations.txt'
-    if (not os.path.exists(filePath)): 
-        import GenerateSampleIntegrations as GSI
-        GSI.main()
+    import sys
     
+    filePath = 'sampleIntegrations.txt'
+
+    for i in range(len(sys.argv)):
+        arg = sys.argv[i]
+        if (arg == '-filePath'):
+            if (i + 1 == len(sys.argv)):
+                print("'-filePath' WAS NOT GIVEN AN ARGUMENT")
+                return
+            filePath = sys.argv[i + 1]
+
+    if (not os.path.exists(filePath)):
+        print("NO SUCH FILE EXISTS:", filePath)
+        return
+        #import GenerateSampleIntegrations as GSI
+        #GSI.main()
+    
+    print('PLOTTING:', filePath)
+
     with open(filePath, 'r') as openFile:
-        dataFrame = pd.read_csv(openFile, engine="pyarrow", header=None, delimiter=',', )
+        dataFrame = pd.read_csv(openFile, engine="pyarrow", header=None, delimiter=',')
 
     timeStamps = dataFrame.iloc[:,0]    
     voltages = dataFrame.iloc[:,1]
