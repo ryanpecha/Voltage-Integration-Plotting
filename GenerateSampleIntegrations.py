91651c659d4a1d04ee285499c1a5dde94f7f2d31
Ryan Pecha <ryanpecha@aol.com>
2022-11-10 08:42:47 -0700

    completed plotting and sample data generation

--- GenerateSampleIntegrations.py
+++ GenerateSampleIntegrations.py
@@ -1,26 +1,27 @@
 
-def main():
 
+
+def generateSampleIntegrations(fileName : str, integrationCount : int, startTime : float, timeStepRange : tuple, voltageRange : tuple) -> None:
     import random
-    
-    integrationCount = 1_000_000
-    fileName = 'sampleIntegrations.txt'
-    startTime = 0
-
-    timeStepRange = (0,0.25)
-    voltageRange = (0,3.5)
-
     currentTime = startTime
     with open('./'+fileName,'w') as openFile:
-
-        for i in range(integrationCount):
+        for _ in range(integrationCount):
             currentTime += random.uniform(timeStepRange[0],timeStepRange[1])
             voltage = random.uniform(voltageRange[0],voltageRange[1])
             line = str(currentTime) + ',' + str(voltage) + '\n'
-            #if (i != integrationCount - 1): line += '\n'
             openFile.write(line)
-        
         openFile.close()
+
+
+
+def main():
+    fileName = 'sampleIntegrations.txt'
+    integrationCount = 10_000
+    startTime = 0
+    timeStepRange = (0.2,0.2)
+    voltageRange = (0,3.5)    
+    generateSampleIntegrations(fileName, integrationCount, startTime, timeStepRange, voltageRange)
+
 
 
 if (__name__ == '__main__'):
--- Main.py
+++ Main.py
@@ -1,32 +1,28 @@
 
 
 
-
-
 def main():
     
-    import sys
-    import matplotlib
-    import numpy
+    import matplotlib.pyplot as plt
     import pandas as pd
+    import os
 
-
-
-    filePath = ''
-
-
-    dataFrame = pd.read_csv(sys.stdin, engine="pyarrow", header=None)
-    columnCount = dataFrame.shape[1]
-
-
+    filePath = 'sampleIntegrations.txt'
+    if (not os.path.exists(filePath)): 
+        import GenerateSampleIntegrations as GSI
+        GSI.main()
     
+    with open(filePath, 'r') as openFile:
+        dataFrame = pd.read_csv(openFile, engine="pyarrow", header=None, delimiter=',', )
 
+    timeStamps = dataFrame.iloc[:,0]    
+    voltages = dataFrame.iloc[:,1]
+    plt.plot(timeStamps,voltages)
 
-
-
-
-
-
+    plt.title('Voltage Integrations')
+    plt.xlabel(f'Time-Stamp SECONDS [ {timeStamps.min()} , {timeStamps.max()} ]')
+    plt.ylabel(f'Voltage [ {voltages.min()} , {voltages.max()} ]')
+    plt.show()
 
 
 
