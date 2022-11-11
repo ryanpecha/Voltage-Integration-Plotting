d82106bf443c6f2f0daa1fbfbbcefb0a38f7f951
Ryan Pecha <ryanpecha@aol.com>
2022-11-10 17:19:12 -0700

    changed indices for mixed sample data

--- Main.py
+++ Main.py
@@ -27,8 +27,95 @@ def main():
     with open(filePath, 'r') as openFile:
         dataFrame = pd.read_csv(openFile, engine="pyarrow", header=None, delimiter=',')
 
-    timeStamps = dataFrame.iloc[:,0]    
-    voltages = dataFrame.iloc[:,1]
+    # simData uses t=0, v=1
+    timeStamps = dataFrame.iloc[:,11]    
+    voltages = dataFrame.iloc[:,0]
+    plt.plot(timeStamps,voltages)
+
+    plt.title('Voltage Integrations')
+    plt.xlabel(f'Time-Stamp SECONDS [ {timeStamps.min()} , {timeStamps.max()} ]')
+    plt.ylabel(f'Voltage [ {voltages.min()} , {voltages.max()} ]')
+    plt.show()
+
+
+
+if (__name__ == '__main__'):
+    main()
+
+
+
+def main():
+    
+    import matplotlib.pyplot as plt
+    import pandas as pd
+    import os
+    import sys
+    
+    filePath = 'sampleIntegrations.txt'
+
+    for i in range(len(sys.argv)):
+        arg = sys.argv[i]
+        if (arg == '-filePath'):
+            if (i + 1 == len(sys.argv)):
+                print("'-filePath' WAS NOT GIVEN AN ARGUMENT")
+                return
+            filePath = sys.argv[i + 1]
+
+    if (not os.path.exists(filePath)):
+        print("NO SUCH FILE EXISTS:", filePath)
+        return
+    
+    print('PLOTTING:', filePath)
+
+    with open(filePath, 'r') as openFile:
+        dataFrame = pd.read_csv(openFile, engine="pyarrow", header=None, delimiter=',')
+
+    # simData uses t=0, v=1
+    timeStamps = dataFrame.iloc[:,11]    
+    voltages = dataFrame.iloc[:,0]
+    plt.plot(timeStamps,voltages)
+
+    plt.title('Voltage Integrations')
+    plt.xlabel(f'Time-Stamp SECONDS [ {timeStamps.min()} , {timeStamps.max()} ]')
+    plt.ylabel(f'Voltage [ {voltages.min()} , {voltages.max()} ]')
+    plt.show()
+
+
+
+if (__name__ == '__main__'):
+    main()
+
+
+
+def main():
+    
+    import matplotlib.pyplot as plt
+    import pandas as pd
+    import os
+    import sys
+    
+    filePath = 'sampleIntegrations.txt'
+
+    for i in range(len(sys.argv)):
+        arg = sys.argv[i]
+        if (arg == '-filePath'):
+            if (i + 1 == len(sys.argv)):
+                print("'-filePath' WAS NOT GIVEN AN ARGUMENT")
+                return
+            filePath = sys.argv[i + 1]
+
+    if (not os.path.exists(filePath)):
+        print("NO SUCH FILE EXISTS:", filePath)
+        return
+    
+    print('PLOTTING:', filePath)
+
+    with open(filePath, 'r') as openFile:
+        dataFrame = pd.read_csv(openFile, engine="pyarrow", header=None, delimiter=',')
+
+    # simData uses t=0, v=1
+    timeStamps = dataFrame.iloc[:,11]    
+    voltages = dataFrame.iloc[:,0]
     plt.plot(timeStamps,voltages)
 
     plt.title('Voltage Integrations')
