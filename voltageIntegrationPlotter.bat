00754d21c81261d60ccfdd796e5181470755746d
Ryan Pecha <ryanpecha@aol.com>
2023-11-17 14:39:30 -0700

    fixed batch args

--- voltageIntegrationPlotter.bat
+++ voltageIntegrationPlotter.bat
@@ -1,4 +1,4 @@
 cd %0"\.."
-::python3.11 "./src/VoltageIntegrationProcessor.py" --hideTerminal %*
-python3.11 "./src/VoltageIntegrationProcessor.py" %1
+python3.11 "./src/VoltageIntegrationProcessor.py" --hideTerminal %1
+::python3.11 "./src/VoltageIntegrationProcessor.py" %1
 pause
